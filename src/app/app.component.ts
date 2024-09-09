d4498c6532ce65df3cae5da5f4caf756fb5e21c5
Batsheva <Btsh4056@gmail.com>
2024-09-09 19:15:28 +0300

    add timer - bring them!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.loadHostageTickerScript();
+  }
+
+  loadHostageTickerScript() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://bringthemhomenow.net/1.1.0/hostages-ticker.js';
+    script.integrity = 'sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne';
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+  }
 }
